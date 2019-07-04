70611b553ecbac81195192a1777f2dac5fafb6af
dakota stuart <dakotastuart@dakotas-MacBook-Pro.local>
2019-07-03 23:11:09 -0700

    added scroll animation

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 // Scroll feature
 $(document).ready(function() {
+    AOS.init();
+
     var page_url = window.location.href;
     var page_id = page_url.substring(page_url.lastIndexOf("#") + 1);
 
